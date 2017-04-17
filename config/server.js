17fc6aa5082dac10495fa67f6ef4814c79359c67
Caio De Albuquerque Lucena Medeiros <camedeiros1@stefanini.com>
2017-04-17 15:00:35 -0300

    Inclusao de sessao para gerenciamento de usuario

--- config/server.js
+++ config/server.js
@@ -10,6 +10,10 @@ var bodyParser = require('body-parser');
 /* importar o módulo do express-validator */
 var expressValidator = require('express-validator');
 
+/* importar o módulo do express-session */
+var expressSession = require('express-session');
+
+
 /* iniciar o objeto do express */
 var app = express();
 
@@ -25,6 +29,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 /* configurar o middleware express-validator */
 app.use(expressValidator());
+
+/* Configura o middleware express-session */
+app.use(expressSession({
+    secret: 'HsIIeoo199986IOirue',
+    resave: false,
+    saveUninitialized: false
+}));
 
 /* efetua o autoload das rotas, dos models e dos controllers para o objeto app */
 consign()

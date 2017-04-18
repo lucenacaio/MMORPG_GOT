18608f84c42f88c2b2aa8ab0e5fdccb029bcd93f
Caio De Albuquerque Lucena Medeiros <camedeiros1@stefanini.com>
2017-04-18 09:02:10 -0300

    inclusao das views de aldeoes e pergaminhos na chamada

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,11 +1,17 @@
 module.exports.jogo = function(application, req, res) {
 
-    if (req.session.autorizado) {
-        res.render('jogo', { img_casa: req.session.casa });
-    } else {
+    if (req.session.autorizado !== true) {
         res.render('index', { validacao: {} });
+        return;
     }
 
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection);
+
+    var usuario = req.session.usuario;
+    var casa = req.session.casa;
+
+    JogoDAO.iniciaJogo(res, usuario, casa);
 
 }
 
@@ -13,4 +19,12 @@ module.exports.sair = function(application, req, res) {
     req.session.destroy(function(err) {
         res.render("index", { validacao: {} });
     });
+}
+
+module.exports.suditos = function(application, req, res) {
+    res.render("aldeoes", { validacao: {} });
+}
+
+module.exports.pergaminhos = function(application, req, res) {
+    res.render("pergaminhos", { validacao: {} });
 }

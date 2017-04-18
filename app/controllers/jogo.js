3fe4f06f524df0f28d6bf0e4203d2ac00e189f72
Caio De Albuquerque Lucena Medeiros <camedeiros1@stefanini.com>
2017-04-18 09:50:23 -0300

    Adicao das acoes de aldeoes no MongoDB

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -5,13 +5,19 @@ module.exports.jogo = function(application, req, res) {
         return;
     }
 
+    var msg = '';
+
+    if (req.query.msg !== '') {
+        msg = req.query.msg;
+    }
+
     var connection = application.config.dbConnection;
     var JogoDAO = new application.app.models.JogoDAO(connection);
 
     var usuario = req.session.usuario;
     var casa = req.session.casa;
 
-    JogoDAO.iniciaJogo(res, usuario, casa);
+    JogoDAO.iniciaJogo(res, usuario, casa, msg);
 
 }
 
@@ -22,9 +28,45 @@ module.exports.sair = function(application, req, res) {
 }
 
 module.exports.suditos = function(application, req, res) {
+    if (req.session.autorizado !== true) {
+        res.render('index', { validacao: {} });
+        return;
+    }
     res.render("aldeoes", { validacao: {} });
 }
 
 module.exports.pergaminhos = function(application, req, res) {
+    if (req.session.autorizado !== true) {
+        res.render('index', { validacao: {} });
+        return;
+    }
     res.render("pergaminhos", { validacao: {} });
+}
+
+module.exports.ordenarAcaoSudito = function(application, req, res) {
+    if (req.session.autorizado !== true) {
+        res.render('index', { validacao: {} });
+        return;
+    }
+
+    let dadosForm = req.body;
+
+    req.assert("acao", "Ação deve ser informada").notEmpty();
+    req.assert("quantidade", "Quantidade deve ser informada").notEmpty();
+
+    let erros = req.validationErrors();
+
+    if (erros) {
+        res.redirect("jogo?msg=A");
+        return;
+    }
+
+    let connection = application.config.dbConnection;
+    let JogoDAO = new application.app.models.JogoDAO(connection);
+
+    dadosForm.usuario = req.session.usuario;
+    JogoDAO.acao(dadosForm);
+
+    res.redirect("jogo?msg=B");
+
 }
